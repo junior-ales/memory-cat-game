eae34b8c6d474cc70d5142d124e5a1748b52d760
Junior Ales <edilson.ales.jr@gmail.com>
2016-10-07 00:14:11 +0100

    version of todo without redux

--- app/initialize.js
+++ app/initialize.js
@@ -1,7 +1,32 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
-import App from 'components/App';
+import { render } from 'react-dom';
+import { compose, map, prop, curry, reduce } from 'ramda';
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.querySelector('#app'));
-});
+const combineComponents = (...args) => {
+  const combine = curry((c, o) => x => (<div>{c(x)} {o(x)}</div>));
+
+  const [first, ...rest] = args;
+  return reduce((acc, c) => combine(acc, c), first, rest);
+};
+
+const state = {
+  year: '2016',
+  title: 'Random stuff',
+  todos: [{id:1, text: 'foo'}, { id:2, text: 'bar'}]
+};
+
+const getTodos = prop('todos');
+
+const Header = title => <h1>A Todo List: {title}</h1>;
+const List = items => <ul>{items}</ul>;
+const Item = todo => <li key={todo.id}>{todo.text}</li>;
+const Footer = text => <div>{text}</div>;
+
+const TodoHeader = compose(Header, s => s.title);
+const TodoList = compose(compose(List, map(Item)), getTodos);
+const TodoFooter = compose(Footer, s => s.year);
+
+const App = combineComponents(TodoHeader, TodoList, TodoFooter);
+
+/* eslint-disable fp/no-unused-expression */
+render(<App {...state} />, document.getElementById('app'));
